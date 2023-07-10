d723477925b0a31dd110ac4708c9f4a0e90d078a
Yeongmin Koh <musicboy9@snu.ac.kr>
2023-07-10 20:16:51 +0900

    Add custom flwr library

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                = 'flwr_custom',
+    version             = '0.1',
+    description         = 'custom flwr 1.4.0 for fl viewer',
+    author              = 'ymkoh',
+    author_email        = 'musicboy9@snu.ac.kr',
+    url                 = 'https://github.com/jeakwon/ccpy',
+    download_url        = 'https://github.com/jeakwon/ccpy/archive/0.0.tar.gz',
+    install_requires    =  [],
+    packages            = find_packages(exclude = []),
+    keywords            = ['ccpy'],
+    python_requires     = '>=3',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
